de71dddfd726faed7d3432f902e83371a332c52c
twoday <1162248131@qq.com>
2021-12-12 17:00:55 +0800

    Update checkiplocal.go

--- checkiplocal.go
+++ checkiplocal.go
@@ -3,13 +3,14 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/gosuri/uitable"
@@ -25,51 +26,68 @@ var opts struct {
 	Addr    bool `short:"a" long:"addr" description:"Print the exact address of the IP by default"`
 }
 
+var sm sync.Map
+
 func main() {
 
 	_, err := flags.ParseArgs(&opts, os.Args)
 	if err != nil || !(opts.Addr || opts.City || opts.Region) {
 		os.Exit(1)
 	}
-	numWorker := opts.Threads
-	results := make(chan map[string]string, numWorker)
-
+	work := make(chan string)
 	go func() {
 		s := bufio.NewScanner(os.Stdin)
 		for s.Scan() {
-			line := s.Text()
-			ip := net.ParseIP(line)
-			if ip == nil {
-				addResults(results, line, "")
-			} else {
-				ret, _ := getIPLocal(ip.String())
-				addResults(results, line, ret)
-			}
+			work <- s.Text()
 		}
 		if err := s.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading standard input:", err)
 		}
-		close(results)
+		close(work)
 	}()
-	table := uitable.New()
-	table.MaxColWidth = 50
-	table.AddRow("IP", "ADDR", "CITY", "REGION")
-	for res := range results {
-		table.AddRow(res["ip"], res["addr"], res["city"], res["region"])
+	wg := &sync.WaitGroup{}
+	for i := 0; i < opts.Threads; i++ {
+		wg.Add(1)
+		go doWork(work, wg)
 	}
+	wg.Wait()
+
+	table := uitable.New()
+	table.MaxColWidth = 80
+	table.AddRow("IP", "ADDR", "CITY", "REGION")
+	sm.Range(func(key, value interface{}) bool {
+		ipInfo := value.(map[string]string)
+		table.AddRow(ipInfo["ip"], ipInfo["addr"], ipInfo["city"], ipInfo["region"])
+		return true
+	})
 	fmt.Println(table)
 }
 
-func addResults(results chan map[string]string, ip, result string) {
+func doWork(work chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for line := range work {
+		ip := net.ParseIP(line)
+		if ip == nil {
+			addResults(line, "")
+		} else {
+			ret, _ := getIPLocal(ip.String())
+			addResults(line, ret)
+		}
+	}
+}
+
+func addResults(ip, result string) {
 	if result == "" {
-		results <- map[string]string{"ip": ip}
+		sm.Store(ip, map[string]string{"ip": ip})
 	} else {
 		ipInfo := make(map[string]string)
 		err := json.Unmarshal([]byte(result), &ipInfo)
 		// 转换失败
 		if err != nil {
-			results <- map[string]string{"ip": ip}
+			sm.Store(ip, map[string]string{"ip": ip})
 		} else {
+
 			if !opts.Addr {
 				ipInfo["addr"] = ""
 			}
@@ -79,7 +97,8 @@ func addResults(results chan map[string]string, ip, result string) {
 			if !opts.Region {
 				ipInfo["region"] = ""
 			}
-			results <- ipInfo
+			ipInfo["addr"] = strings.TrimSpace(ipInfo["addr"])
+			sm.Store(ip, ipInfo)
 		}
 	}
 }
@@ -92,15 +111,11 @@ func getIPLocal(ip string) (string, error) {
 	}
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
 	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: false,
-			},
-		},
 		Timeout: 30 * time.Second,
 	}
 	resp, err := client.Do(req)
 	if err != nil {
+		fmt.Println(err)
 		return "", err
 	}
 	//关闭resp,避免内存泄露
